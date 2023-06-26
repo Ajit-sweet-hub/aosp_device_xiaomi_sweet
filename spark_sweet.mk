2c98102eaae7abad1501921d134ba1d52bb213bd
ajitlenka30 <ajitlenka30@gmail.com>
2023-06-26 19:50:50 +0530

    test

--- spark_sweet.mk
+++ spark_sweet.mk
@@ -6,16 +6,17 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
 # Inherit some common stuff
-$(call inherit-product, vendor/spark/config/common_full_phone.mk)
+$(call inherit-product, vendor/spark/config/common.mk)
 
 # Bootanimation Resolution
 TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_USES_BLUR := true
 
 # FaceUnlock
 TARGET_FACE_UNLOCK_SUPPORTED := true
 
 # Reduce system image size by limiting java debug info.
-PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
+#PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
 
 # Gapps
 WITH_GAPPS=true
